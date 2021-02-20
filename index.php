494fab74244c4dd5463245a240b623b11c85ae5a
Ian Horris <harriian@gmail.com>
2021-02-19 21:10:35 -0500

    My first commit! Attempting to share my first website I created on my own from scratch

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<title> Pi'erre Bourne Forum </title>
+        <!--<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">-->
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+
+    <body>
+    <nav>
+    	<p> Exclusives Mp3's </p>
+    	<ul>
+    		<li><a href="index.php">Home</a>
+    		<li><a href="#">Artists</a>
+    			<ul>
+    				<li><a href="pierre.html">Pi'erre Bourne</a></li>
+    				<li><a href="uzi.html">Lil Uzi Vert</a></li>
+    				<li><a href="carti.html">Playboi Carti</a></li>
+    			</ul>
+    	</ul>
+    </nav><br><br>
+
+    
+    </body>
+
+</html>
